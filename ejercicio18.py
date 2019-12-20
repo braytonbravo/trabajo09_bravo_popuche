99e252015f6e3b01053dc7056e558429f7c765ad
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-20 10:32:56 -0500

    se desarrollo el ejercicio18

---
+++ ejercicio18.py
@@ -0,0 +1,12 @@
+import os
+import libreria
+
+# EJERCICIO 18
+#DECLARCION DE VARIABLES
+
+# input
+lado=int(os.sys.argv[1])
+cuadrado=libreria.cuadrado(lado)
+
+# output
+print("El area del cuadrado es:", cuadrado)
