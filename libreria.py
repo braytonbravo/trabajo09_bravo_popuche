4f4e3841489b8fecabf7df92754ce03e86a6aa59
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-20 10:37:57 -0500

    se desarrollo libreria

---
+++ libreria.py
@@ -0,0 +1,149 @@
+# AREA DEL RECTANGULO
+def area_rectangulo(base, altura):
+    resultado=(base*altura)
+    return resultado
+#fin_def
+
+# PROMEDIO DE TRES NOTAS
+def promedio(nota1, nota2, nota3):
+    res=(nota1+nota2+nota3)/3
+    return res
+#fin_def
+
+# HALLAR EL TRABAJO
+def trabajo(fuerza, distancia):
+    resu=(fuerza*distancia)
+    return resu
+#fin_def
+
+# HALLAR PRECIO DE VENTA
+def precio_venta(precio_compra, ganancia):
+    pv=(precio_compra+ganancia)
+    return pv
+#fin_def
+
+# HALLAR LA FORMULA FISICA DE LA POTENCIA
+def potencia(trabajo, tiempo):
+    poten=(trabajo/tiempo)
+    return poten
+#fin_def
+
+# HALLAR EL AREA DE TRAPECIO
+def area_trapecio(base_menor, base_mayor, h):
+    area_tra=((base_menor+base_mayor)/2)*h
+    return area_tra
+#fin_def
+
+# HALLAR EL AREA DEL CIRCULO
+def area_circulo(pi, radio):
+    circulo=(pi*radio**2)
+    return circulo
+#fin_def
+
+# PROMEDIO GEOMETRICO DE DOS NUMEROS
+def prom_geometrico(a, b):
+    pg=(a*b)**0.5
+    return pg
+#fin_def
+
+# HALLAR EL PROMEDIO ARMONICO DE DOS NUMEROS
+def pro_armonico(x, y):
+    p_armonico=(2*x*y)/(x+y)
+    return p_armonico
+#fin_def
+
+# HALLAR LA HIPOTENUSA DE UN TRIANGULO RECTANGULO
+def hipotenusa(ca, co):
+    hipote=(ca**2 + co**2)
+    return hipote
+#fin_def
+
+# HALLAR LA ACELERACION DE UN VEHICULO
+def aceleracion(fuerza, masa):
+    acelera=(fuerza/masa)
+    return acelera
+#fin_def
+
+#  HALLAR LA VELOCIDAD DE UN BICICLETA
+def velocidad(distancia, tiempo):
+    veloci=(distancia/tiempo)
+    return veloci
+#fin_def
+
+# HALLAR LA GANANCIA DE UN PRODUCTO
+def ganancia(p_venta, p_compra):
+    gana=(p_venta-p_compra)
+    return  gana
+#fin_def
+
+# HALLAR LA VELOCIDAD FINAL DE UN AUTO
+def velocidad_final(vi, aceleracion, tiempo):
+    vf=(vi-(aceleracion*tiempo))
+    return vf
+#fin_def
+
+# HALLAR LA ALTURA DE UN EDIFICIO
+def altura(vo, vf, t):
+    h=(vo+vf)/2*t
+    return h
+#fin_def
+
+# HALLA LA LA TANGENTE DE UN ANGULO
+def tangente(cateto_opuesto, cataeto_adyacente):
+    tg=(cateto_opuesto/cataeto_adyacente)
+    return tg
+#fin_def
+
+# HALLAR EL VOLTAJE
+def voltaje(resitencia, intensidad):
+    vol=(resitencia*intensidad)
+    return vol
+#fin_def
+
+# HALLAR EL AREA DEL CUADRADO
+def cuadrado(lado):
+    cuadra=(lado**2)
+    return cuadra
+#fin_def
+
+# HALLAR EL VOLUMEN DEL PARARLEPIPEDO
+def volumen_paralepipedo(c, d, e):
+    paralepi=(c*d*e)
+    return  paralepi
+#fin_def
+
+# AREA DEL ROMBO
+def rombo(diagonal_mayor, diagonal_menor):
+    rom=(diagonal_mayor*diagonal_menor)/2
+    return rom
+#fin_def
+
+# HALLLAR EL TIEMPO QUE RECORRE UN AUTOMOVIL
+def tiempo(espacio, rapidez):
+    tiem=(espacio/rapidez)
+    return  tiem
+#fin_def
+
+# HALLAR EL AREA LATERAL DEL PRISMA
+def area_lateral(perimetro_base, altura):
+    lateral=(perimetro_base*altura)
+    return lateral
+#fin_def
+
+# HALLAR EL CALOR
+def calor(mas, temp, c_espe):
+    calor=(mas*temp*c_espe)
+    return calor
+#fin_def
+
+# hallar el costo fijo medio
+def medio(fijo, produccion):
+    medio=(fijo/produccion)
+    return medio
+#fin_def
+
+#  HALLAR EL AREA DEL TRIANGULO
+def triangulo(base, altura):
+    triangulo=(base*altura)
+    return triangulo
+#fin_def
