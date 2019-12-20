91e67d1e417223d19a687d9298847420903e33c3
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-20 10:38:24 -0500

    se desarrollo lote.bat

---
+++ lote.bat
@@ -0,0 +1,25 @@
+python ejercicio1.py             40.5  5
+python ejercicio2.py             12 14 16
+python ejercicio3.py             30 17
+python ejercicio4.py             12.8 4.5
+python ejercicio5.py             43 12
+python ejercicio6.py             38 36
+python ejercicio7.py             56 24
+python ejercicio8.py             23.5 12.6
+python ejercicio9.py             34 27
+python ejercicio10.py            89 72
+python ejercicio11.py            25 5
+python ejercicio12.py            42.6 34.9
+python ejercicio13.py            45.8 9.7
+python ejercicio14.py            31.9 72.3 45.8
+python ejercicio15.py            43 12 21
+python ejercicio16.py            24 16
+python ejercicio17.py            17 18
+python ejercicio18.py            24
+python ejercicio19.py            10 30 40
+python ejercicio20.py            12 21
+python ejercicio21.py            16 8
+python ejercicio22.py            16 19 21
+python ejercicio23.py            32 45 68
+python ejercicio24.py            35 7
+python ejercicio25.py            14 6
