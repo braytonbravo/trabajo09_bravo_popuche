0560119822307f655f0df9e26899996eb2200d87
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-20 10:28:28 -0500

    se desarrollo el ejercicio3

---
+++ ejercicio3.py
@@ -0,0 +1,15 @@
+import os
+import libreria
+
+# ejercicio 03
+# HALLAR EL TRABAJO
+# DECLARACION DE VARIABLES
+fuerza,distancia,trabajo=0,0,0
+
+# input
+fuerza=int(os.sys.argv[1])
+distancia=int(os.sys.argv[2])
+trabajo=libreria.trabajo(base,altura)
+
+# output
+print("EL trabajo es:", trabajo)
